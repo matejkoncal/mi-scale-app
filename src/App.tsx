d75c27467a4125f39fbe9025081166c662567732
Matej Končal <matej@koncal.sk>
2024-05-01 21:29:01 +0200

    small fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,23 @@ import { Button } from '@mui/material';
 import './App.css'
 import { useMiScale } from './useMiScale/useMiScale'
 import ReactSpeedometer from "react-d3-speedometer"
+import { useEffect } from 'react';
 
 function App() {
   const { startScan, debouncedWeight } = useMiScale();
-  document.onvisibilitychange = () => {
-    if (document.visibilityState === 'visible') {
-      startScan();
+
+  useEffect(() => {
+    document.onvisibilitychange = () => {
+      if (document.visibilityState === 'visible') {
+        startScan();
+      }
     }
-  }
+  }, [startScan]);
 
   return (
     <>
       <ReactSpeedometer minValue={0} maxValue={160} segments={1} currentValueText="${value} Kg" value={debouncedWeight} />
-      <Button onClick={startScan} variant="contained" color="primary">Scan</Button>
+      <Button onClick={startScan} variant="contained" color="primary">Try connect</Button>
     </>
   )
 }
